1465050969a7cff186842fc3e062d94237e7a1fb
kukorra <154669365+kukorra@users.noreply.github.com>
2025-06-16 02:41:10 +0500

    Update lab3.pl

--- lab3.pl
+++ lab3.pl
@@ -1,24 +1,19 @@
-﻿
-unique([], []).
+﻿unique([], []).
 unique([H|T], [H|T2]) :- not(member(H, T)), unique(T, T2).
 unique([H|T], T2) :- member(H, T), unique(T, T2).
 
-% Объединение двух списков
 union([], L, L).
 union([H|T], L, [H|T2]) :- not(member(H, L)), union(T, L, T2).
 union([H|T], L, T2) :- member(H, L), union(T, L, T2).
 
-% Пересечение двух списков
 intersection([], _, []).
 intersection([H|T], L, [H|T2]) :- member(H, L), intersection(T, L, T2).
 intersection([H|T], L, T2) :- not(member(H, L)), intersection(T, L, T2).
 
-% Дополнение множества A относительно множества U (все элементы, которые есть в U, но нет в A)
 complement([], _, []).
 complement([H|T], U, [H|T2]) :- not(member(H, U)), complement(T, U, T2).
 complement([H|T], U, T2) :- member(H, U), complement(T, U, T2).
 
-% Закон де Моргана: ~ (A U B) = (~ A) ∩ (~ B)
 de_morgan(A, B) :-
     unique(A, A1),
     unique(B, B1),
@@ -29,7 +24,6 @@ de_morgan(A, B) :-
     intersection(NotA, NotB, NotUnionAB),
     write('Закон де Моргана выполняется: ~ (A U B) = (~ A) ∩ (~ B)'), nl.
 
-% Стартовая процедура для тестирования с вводом с клавиатуры
 start :-
     write('Введите множество A (список чисел): '), nl,
     read(A), % Чтение множества A с клавиатуры
